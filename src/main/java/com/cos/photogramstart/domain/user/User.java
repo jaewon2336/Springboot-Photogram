3c60ddd40c745318cab724a212c40e026e481963
jaewon <xldzjqpf1588@naver.com>
2023-01-16 23:08:50 +0900

    회원가입 - User 모델 만들기

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.cos.photogramstart.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// JPA(Java Persistance API) : 자바로 데이터를 DB에 영구적으로 저장할 수 있는 API를 제공
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity // DB에 테이블을 생성
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략 : 데이터베이스 설정을 따라감
+    private int id;
+
+    private String username;
+    private String password;
+
+    private String name;
+    private String website; // 웹 사이트
+    private String bio; // 자기소개
+    private String email;
+    private String phone;
+    private String gender;
+
+    private String profileImageUrl; // 프로필사진
+    private String role; // 권한
+
+    private LocalDateTime createDate;
+
+    @PrePersist // DB에 INSERT되기 직전에 실행
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,24 +1,31 @@
 package com.cos.photogramstart.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller // IoC, 파일 리턴 컨트롤러 
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+
+@Controller // IoC, 파일 리턴 컨트롤러
 public class AuthController {
-	
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
 	@GetMapping("/auth/signin")
 	public String signinForm() {
 		return "auth/signin";
 	}
-	
+
 	@GetMapping("/auth/signup")
 	public String signupForm() {
 		return "auth/signup";
 	}
-	
+
 	@PostMapping("/auth/signup")
-	public String signup() {
+	public String signup(SignupDto signupDto) { // key=value(x-www-form-urlencoded)
+		log.info("디버그 : " + signupDto.toString());
 		return "auth/signin";
 	}
 
