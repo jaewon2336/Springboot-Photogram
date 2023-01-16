a54fdd33ab99f54acc76fba37b35259fce6e13a3
jaewon <xldzjqpf1588@naver.com>
2023-01-16 22:19:38 +0900

    회원가입 - SecurityConfig 생성

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화 
+@Configuration // IoC   
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// super.configure(http); // 시큐리티가 로그인 페이지 가로채는 이유 
+		// super 삭제 시 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨 
+		
+		http.authorizeRequests()
+			.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin")
+			.defaultSuccessUrl("/");
+	}
+}
