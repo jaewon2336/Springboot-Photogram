5b256d55c3762f75580dbc56603709c31f78e786
jaewon <xldzjqpf1588@naver.com>
2023-01-24 13:22:00 +0900

    회원가입 - 비밀번호 암호화 및 username 유니크 제약조건 추가

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,27 +1,34 @@
 package com.cos.photogramstart.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity // 해당 파일로 시큐리티를 활성화 
-@Configuration // IoC   
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화
+@Configuration // IoC
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// super.configure(http); // 시큐리티가 로그인 페이지를 가로채는 이유 
-		// super 삭제 시 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨 
-		
-		http.csrf().disable(); // CSRF 토큰 비활성화 
-		
+		// super.configure(http); // 시큐리티가 로그인 페이지를 가로채는 이유
+		// super 삭제 시 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨
+
+		http.csrf().disable(); // CSRF 토큰 비활성화
+
 		http.authorizeRequests()
-			.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
-			.anyRequest().permitAll()
-			.and()
-			.formLogin()
-			.loginPage("/auth/signin")
-			.defaultSuccessUrl("/");
+				.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/auth/signin")
+				.defaultSuccessUrl("/");
 	}
 }
--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -25,6 +26,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략 : 데이터베이스 설정을 따라감
     private int id;
 
+    @Column(unique = true)
     private String username;
     private String password;
 
